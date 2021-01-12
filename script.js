670475946a35b1863f23363a4ccab04aac61c01a
Laura Salinas <laura.salinas@oktana.io>
2021-01-11 21:54:47 -0300

    fixing the event

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+const btn = document.getElementById('button-add');
+
 function addItem() {
-    let task = document.getElementById("task").value;
+    let task = document.getElementById('task').value;
     let node = document.createTextNode(task);
 
-    let itemNew = document.createElement("li.collection-item");
+    let itemNew = document.createElement('li.collection-item');
     itemNew.appendChild(node);
 
-    let element = document.getElementById("collection-list");
+    let element = document.getElementById('collection-list');
     element.appendChild(itemNew);
+
+    task.value = '';
 }
+
+btn.addEventListener('click', addItem);
