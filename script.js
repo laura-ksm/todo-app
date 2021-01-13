56c310e2cf6e3b77e30381e014ebe82a5ab1d3ac
Laura Salinas <laura.salinas@oktana.io>
2021-01-13 08:07:33 -0300

    insert childs in the list

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const btn = document.getElementById('button-add');
+let btn = document.getElementById('button-add');
 btn.addEventListener('click', () => {
+    let iconText = document.createTextNode('delete');
+    let iconDelete = document.createElement('i');
+    iconDelete.className = 'material-icons';
+    iconDelete.appendChild(iconText);
+
+    let btnRemove = document.createElement('a');
+    btnRemove.className = 'secondary-content';
+    btnRemove.setAttribute('href','#!');
+    btnRemove.appendChild(iconDelete);
+
     let task = document.getElementById('task').value;
     let node = document.createTextNode(task);
 
-    let itemNew = document.createElement('li.collection-item');
+    let itemNew = document.createElement('li');
+    itemNew.className = 'collection-item';
     itemNew.appendChild(node);
+    itemNew.appendChild(btnRemove);
 
     let element = document.getElementById('collection-list');
     element.appendChild(itemNew);
@@ -13,4 +25,12 @@ btn.addEventListener('click', () => {
 let btnClear = document.getElementById('button-add');
 btnClear.addEventListener('click', () => {
     let inputTask = document.getElementById('task').value = '';
+});
+
+let btnDelete = document.getElementById('button-delete');
+btn.addEventListener('click', () => {
+    aux = document.getElementById('button-delete');
+    child = aux.parentNode;
+    parent = document.getElementById('collection-list');
+    parent.removeChild(child);
 });
